d260d65cd19bdfc86a09dec5ffd18e840129f4d2
Anna <annabagdevic@gmail.com>
2024-12-13 16:19:10 +0300

    change smth

--- image/ImageProcessor.py
+++ image/ImageProcessor.py
@@ -3,14 +3,14 @@ from image.ImageHandler import ImageHandler
 
 class ImageProcessor():
 
-    def __init__(self, image_handler):
-        self.image_handler = image_handler
+    def __init__(self, filter_image):
+        self.filter_image = filter_image
 
     def add_filter(self):
-        self.image_handler.original_img = self.image_handler.original_img.filter(ImageFilter.CONTOUR)
+        self.filter_image.original_img = self.filter_image.original_img.filter(ImageFilter.CONTOUR)
 
     def add_text(self, position, text, size, color):
-        draw_text = ImageDraw.Draw(self.image_handler.original_img)
+        draw_text = ImageDraw.Draw(self.filter_image.original_img)
         font = ImageFont.truetype('arial.ttf', size)
         draw_text.text(position, text, font= font, fill = color)
 
